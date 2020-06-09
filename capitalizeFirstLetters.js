f9114efdcc0b8e9899bb689f4631c27a06b26651
thierry-a <thierryarzal353@gmail.com>
2020-06-09 06:01:56 +0200

    Ecriture du code refactoriser

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -3,15 +3,9 @@ const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirst(input) { 
-    let output = '';
-    for (let i = 0 ; i < input.length ; i++) {
-      if (i === 0) {
-        output += input[0].toUpperCase();
-      } else {
-        output += input[i];
-      }
-    }
-    return output;
+    return input.length > 0
+        ? input[0].toUpperCase() + input.slice(1)
+        : '';
 }
 
 // Check that capitalizeFirst is a function
