944f1215344cc601677cc170985acc5f2f1d33c9
thierry-a <thierryarzal353@gmail.com>
2020-06-09 07:19:20 +0200

    Reécriture des tests pour utilisation avec mocha

---
+++ test/capitalizeFirst.test.js
@@ -0,0 +1,20 @@
+// capitalizeFirst.js
+const assert = require('assert');
+const capitalizeFirst = require('../capitalizeFirst');
+
+describe('capitalizeFirst', () => {
+    it('is a function that accepts one argument', () => {
+      assert.strictEqual(typeof capitalizeFirst, 'function');
+      assert.strictEqual(capitalizeFirst.length, 1);
+    });
+    it('transforms javaScript correctly', () => {
+      assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+    });
+    it('works for a 1-character string', () => {
+      assert.strictEqual(capitalizeFirst('z'), 'Z');
+    });
+    it('works for an empty string', () => {
+      assert.strictEqual(capitalizeFirst(''), '');
+    });
+  });
+  
